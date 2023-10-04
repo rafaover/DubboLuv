1f3671c5279dccf168d97b82697ec5289e8adc7a
rafaover <rafamsilva@gmail.com>
2023-10-04 11:56:42 +1100

    CategoryData.kt - Create basic file for categories data.

---
+++ app/src/main/java/com/feeltheboard/dubboluv/data/CategoryData.kt
@@ -0,0 +1,70 @@
+package com.feeltheboard.dubboluv.data
+
+import com.feeltheboard.dubboluv.model.Category
+
+object CategoryData {
+    fun getCategoriesData(): List<Category> {
+        return listOf(
+            Category(
+                id = 1,
+                categoryTitleId = com.feeltheboard.dubboluv.R.string.category1_asianfood,
+                categoryIconId = com.feeltheboard.dubboluv.R.drawable.asian_food_24,
+//                destinations = DestinationData.getDestinationsData()
+            ),
+            Category(
+                id = 2,
+                categoryTitleId = com.feeltheboard.dubboluv.R.string.category2_bakery,
+                categoryIconId = com.feeltheboard.dubboluv.R.drawable.bakery_24,
+//                destinations = DestinationData.getDestinationsData()
+            ),
+            Category(
+                id = 3,
+                categoryTitleId = com.feeltheboard.dubboluv.R.string.category3_breakkie,
+                categoryIconId = com.feeltheboard.dubboluv.R.drawable.breakfast_24,
+//                destinations = DestinationData.getDestinationsData()
+            ),
+//            Category(
+//                id = 4,
+//                categoryTitleId = com.feeltheboard.dubboluv.R.string.category4_butcher,
+//                categoryIconId = com.feeltheboard.dubboluv.R.drawable.ic_category_4,
+//                destinations = DestinationData.getDestinationsData()
+//            ),
+//            Category(
+//                id = 5,
+//                categoryTitleId = com.feeltheboard.dubboluv.R.string.category5_burger,
+//                categoryIconId = com.feeltheboard.dubboluv.R.drawable.ic_category_5,
+//                destinations = DestinationData.getDestinationsData()
+//            ),
+//            Category(
+//                id = 6,
+//                categoryTitleId = com.feeltheboard.dubboluv.R.string.category6_coffeespot,
+//                categoryIconId = com.feeltheboard.dubboluv.R.drawable.ic_category_6,
+//                destinations = DestinationData.getDestinationsData()
+//            ),
+//            Category(
+//                id = 7,
+//                categoryTitleId = com.feeltheboard.dubboluv.R.string.category7_kidspark,
+//                categoryIconId = com.feeltheboard.dubboluv.R.drawable.ic_category_7,
+//                destinations = DestinationData.getDestinationsData()
+//            ),
+//            Category(
+//                id = 8,
+//                categoryTitleId = com.feeltheboard.dubboluv.R.string.category8_Lunch,
+//                categoryIconId = com.feeltheboard.dubboluv.R.drawable.ic_category_8,
+//                destinations = DestinationData.getDestinationsData()
+//            ),
+//            Category(
+//                id = 9,
+//                categoryTitleId = com.feeltheboard.dubboluv.R.string.category9_pizza,
+//                categoryIconId = com.feeltheboard.dubboluv.R.drawable.ic_category_9,
+//                destinations = DestinationData.getDestinationsData()
+//            ),
+//            Category(
+//                id = 10,
+//                categoryTitleId = com.feeltheboard.dubboluv.R.string.category10_pub,
+//                categoryIconId = com.feeltheboard.dubboluv.R.drawable.ic_category_10,
+//                destinations = DestinationData.getDestinationsData()
+//            ),
+        )
+    }
+}
