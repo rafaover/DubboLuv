d1fec8a9f31fc74514ef878aef8a4ddafa6f9380
rafaover <rafamsilva@gmail.com>
2023-10-05 09:39:15 +1100

    Initial Home Screen composable file: - Added main composable DubboLuvApp. - Created DubboLuvAppTopBar using CenterAlignedTopAppBar - Created base DubboLuvAppContent.(Incomplete)

---
+++ app/src/main/java/com/feeltheboard/dubboluv/ui/HomeScreen.kt
@@ -0,0 +1,63 @@
+package com.feeltheboard.dubboluv.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.feeltheboard.dubboluv.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DubboLuvApp() {
+    Scaffold (
+        topBar = {
+            DubboLuvAppTopBar()
+        },
+        content = {
+            DubboLuvAppContent(paddingValues = it)
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DubboLuvAppTopBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(R.drawable.baseline_location_city_24),
+                    contentDescription = stringResource(R.string.app_name),
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_small))
+                        .size(dimensionResource(R.dimen.image_size))
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayMedium
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun DubboLuvAppContent(
+    paddingValues: PaddingValues,
+    modifier: Modifier = Modifier
+) {
+    TODO("Lazy Column with the categories and their data.")
+}
