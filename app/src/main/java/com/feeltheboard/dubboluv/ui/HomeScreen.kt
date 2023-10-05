f2e2096dc47dc534e83816ac764223f09ce10598
rafaover <rafamsilva@gmail.com>
2023-10-05 13:15:39 +1100

    Updated the call for the content in the HomeScreen.

--- app/src/main/java/com/feeltheboard/dubboluv/ui/HomeScreen.kt
+++ app/src/main/java/com/feeltheboard/dubboluv/ui/HomeScreen.kt
@@ -1,12 +1,11 @@
 package com.feeltheboard.dubboluv.ui
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.feeltheboard.dubboluv.ui.components.DubboLuvAppTopBar
+import com.feeltheboard.dubboluv.ui.components.HomeScreenCategoryList
 import com.feeltheboard.dubboluv.ui.theme.DubboLuvTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -17,17 +16,9 @@ fun DubboLuvApp() {
             DubboLuvAppTopBar()
         },
         content = {
-            DubboLuvAppContent(paddingValues = it)
+            HomeScreenCategoryList(contentPadding = it)
         }
     )
-}
-
-@Composable
-fun DubboLuvAppContent(
-    paddingValues: PaddingValues,
-    modifier: Modifier = Modifier
-) {
-    TODO("Lazy Column with the categories and their data.")
 }
 
 @Preview(showBackground = true)
