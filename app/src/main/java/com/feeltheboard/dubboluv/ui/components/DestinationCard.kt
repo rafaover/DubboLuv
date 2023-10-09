677d9ab70cc32823658e8473e6accd9e99cdb5b3
rafaover <rafamsilva@gmail.com>
2023-10-09 13:31:51 +1100

    Created DestinationCard (Basic UI, update later)

---
+++ app/src/main/java/com/feeltheboard/dubboluv/ui/components/DestinationCard.kt
@@ -0,0 +1,59 @@
+package com.feeltheboard.dubboluv.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.feeltheboard.dubboluv.R
+import com.feeltheboard.dubboluv.model.Destination
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DestinationCard(
+    onCardClick: (Destination) -> Unit,
+    destination: Destination,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        onClick = { onCardClick(destination) }
+    ) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_small)),
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+                Image(
+                    modifier = modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small))
+                        .clip(MaterialTheme.shapes.small),
+                    painter = painterResource(destination.destinationImageId),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = stringResource(destination.titleId)
+                )
+                Text(
+                    text = stringResource(destination.titleId),
+                    style = MaterialTheme.typography.displayMedium,
+                )
+            }
+        }
+    }
+}
