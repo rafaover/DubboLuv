560895905db277b69051817b124803f13548b1ab
rafaover <rafamsilva@gmail.com>
2023-10-09 13:33:15 +1100

    Updataed HomeScreenCategoryList.kt to maintain UDF

--- app/src/main/java/com/feeltheboard/dubboluv/ui/components/HomeScreenCategoryList.kt
+++ app/src/main/java/com/feeltheboard/dubboluv/ui/components/HomeScreenCategoryList.kt
@@ -1,31 +1,27 @@
 package com.feeltheboard.dubboluv.ui.components
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import com.feeltheboard.dubboluv.R
 import com.feeltheboard.dubboluv.data.categoryList
 import com.feeltheboard.dubboluv.model.Category
 
 @Composable
 fun HomeScreenCategoryList(
-    contentPadding: PaddingValues
+    contentPadding: PaddingValues,
+    onClick: (Category) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     LazyColumn(
         contentPadding = contentPadding
     ) {
-        items(categoryList) { category ->
+        items(categoryList) {
             CategoryCard(
-                category = category,
-                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
-                onCardClick = {
-                    TODO("Navigate to the category screen with List of Destinations")
-                }
-
+                category = it,
+                modifier = modifier,
+                onCardClick = onClick
             )
         }
     }
