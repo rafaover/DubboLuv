16235777b6fea51e3918f544eb3f6e027feab12c
rafaover <rafamsilva@gmail.com>
2023-10-09 13:51:29 +1100

    Added arguments to new parameters from DubboLuvAppTopBar and HomeScreenCategoryList Composables. Maintaining UDF.

---
+++ app/src/main/java/com/feeltheboard/dubboluv/ui/MainScreen.kt
@@ -0,0 +1,58 @@
+package com.feeltheboard.dubboluv.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.feeltheboard.dubboluv.R
+import com.feeltheboard.dubboluv.ui.components.DubboLuvAppTopBar
+import com.feeltheboard.dubboluv.ui.components.HomeScreenCategoryList
+import com.feeltheboard.dubboluv.ui.components.SelectedCategoryScreen
+import com.feeltheboard.dubboluv.ui.theme.DubboLuvTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DubboLuvApp() {
+    val viewModel: CategoriesViewModel = CategoriesViewModel()
+    val uiState by viewModel.uiState.collectAsState()
+    Scaffold (
+        topBar = {
+            DubboLuvAppTopBar(
+                isShowingCategoryList = uiState.isShowingCategoryList,
+                onBackButtonClick = {
+                    viewModel.navigateToCategoryListPage()
+                }
+            )
+        }
+    ) { innerPadding ->
+        HomeScreenCategoryList(
+            contentPadding = innerPadding,
+            modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
+            onClick = {
+                viewModel.updateCurrentCategory(it)
+                viewModel.navigateToSelectedCategoryPage()
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DubboLuvAppPreview() {
+    DubboLuvTheme(darkTheme = false) {
+        DubboLuvApp()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DubboLuvAppPreviewDarkTheme() {
+    DubboLuvTheme(darkTheme = true) {
+        DubboLuvApp()
+    }
+}
