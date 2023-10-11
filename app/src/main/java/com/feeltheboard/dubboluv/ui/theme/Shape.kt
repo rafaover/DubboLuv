5528abee8011d025b21ef3c80e9ef515f8f3eb50
rafaover <rafamsilva@gmail.com>
2023-10-11 11:31:42 +1100

    Shape.kt - Updated shape values.

--- app/src/main/java/com/feeltheboard/dubboluv/ui/theme/Shape.kt
+++ app/src/main/java/com/feeltheboard/dubboluv/ui/theme/Shape.kt
@@ -12,8 +12,9 @@ val Shapes = Shapes(
     /** The main goal of this shape is to target the CARD composable.
     The card composable by default is a medium shape. So you do not need to specify it there.
     It will be applied automatically. */
-    medium = RoundedCornerShape(bottomStart = 16.dp),
+//    medium = RoundedCornerShape(bottomStart = 12.dp),
+    medium = RoundedCornerShape(12.dp),
 
     /** The default shape for the Selected Category Destinations Cards composable. */
-    large = RoundedCornerShape(16.dp)
+    large = RoundedCornerShape(12.dp)
 )
