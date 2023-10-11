64a3bb88b5a22f970988948df48ea2b2dae4735f
rafaover <rafamsilva@gmail.com>
2023-10-11 13:22:55 +1100

    Updated card. - Updated the modifier parent-child relation inside image.

--- app/src/main/java/com/feeltheboard/dubboluv/ui/components/DestinationCard.kt
+++ app/src/main/java/com/feeltheboard/dubboluv/ui/components/DestinationCard.kt
@@ -1,57 +1,75 @@
 package com.feeltheboard.dubboluv.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.feeltheboard.dubboluv.R
 import com.feeltheboard.dubboluv.model.Destination
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DestinationCard(
     destination: Destination,
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .height(dimensionResource(R.dimen.destination_card_height)),
+        elevation = CardDefaults.elevatedCardElevation()
     ) {
-        Column {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(dimensionResource(R.dimen.padding_small)),
-                verticalAlignment = Alignment.CenterVertically
-
-            ) {
+        Box {
+            Column {
                 Image(
-                    modifier = modifier
-                        .size(dimensionResource(R.dimen.image_size))
-                        .padding(dimensionResource(R.dimen.padding_small))
-                        .clip(MaterialTheme.shapes.small),
                     painter = painterResource(destination.destinationImageId),
                     contentScale = ContentScale.Crop,
-                    contentDescription = stringResource(destination.titleId)
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(dimensionResource(id = R.dimen.destination_image_height))
+                        .clip(RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp)),
                 )
                 Text(
                     text = stringResource(destination.titleId),
-                    style = MaterialTheme.typography.displayMedium,
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_medium)),
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+
                 )
             }
         }
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun DestinationCardPreview() {
+    DestinationCard(
+        destination = Destination(
+            id = 1,
+            destinationImageId = R.drawable.rosegarden,
+            titleId = R.string.rosegarden_title,
+            descriptionId = R.string.rosegarden_description,
+            addressId = R.string.rosegarden_address,
+        )
+    )
 }
