c1d8636aaf05d76b26c01da22a3a769fe1e15e1c
rafaover <rafamsilva@gmail.com>
2023-10-12 11:37:11 +1100

    ADded WindowSizeClass for adaptive layout - added dependency - Created Enum class for windowSize types

---
+++ app/src/main/java/com/feeltheboard/dubboluv/utils/ScreenContentType.kt
@@ -0,0 +1,9 @@
+package com.feeltheboard.dubboluv.utils
+
+/**
+ * Content shown depending on size and state of device.
+ */
+enum class ScreenContentType {
+    CATEGORIES,
+    CATEGORIES_AND_DESTINATIONS
+}
